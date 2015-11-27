7deec8e91c64437c654e9cac9cf6db27b1dba425
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2015-11-27 17:17:41 +0900

    Convert EVEX standoff packages

---
+++ tools/convert-evex.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Convert EVEX standoff data to PubAnnotation JSON.
+
+# Assumes .tar.gz packages as input, creates .zip
+
+set -u
+set -e
+set -o pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OUTDIR="$( pwd )"
+SO2PA="$SCRIPTDIR/../standoff2pa.py"
+TYPEMAP="$SCRIPTDIR/../typemap-evex.txt"
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 FILE [FILE [...]]" >&2
+    exit 1
+fi
+
+TMPDIR=`mktemp -d tmp.XXXXXXXXXX`
+function cleanup {
+    rm -rf "$TMPDIR"
+}
+trap cleanup EXIT
+
+for f in "$@"; do
+    echo "Unpacking $f ..." >&2
+    tar xzf "$f" -C "$TMPDIR"
+
+    echo "Converting $f ..." >&2
+    find "$TMPDIR" -name '*.ann' | \
+	xargs python "$SO2PA" -m "$TYPEMAP" -o "$TMPDIR"
+    
+    o=`basename "$f" .tar.gz`.zip
+    echo "Packaging into $o ..." >&2
+    cd "$TMPDIR"
+    ls | egrep '\.json$' | xargs zip -q "$OUTDIR/$o"
+    cd ..
+
+    echo "Cleaning up for $f ..." >&2
+    rm -rf "$TMPDIR"
+    mkdir "$TMPDIR"
+done
